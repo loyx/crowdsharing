c4b0813db327cd5f8f696e733b927eed1774581a
loyx <isluoyixuan1@163.com>
2021-01-12 22:03:36 +0800

    发布任务前端接受任务前端

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -9,7 +9,7 @@ window.onload = function(){
 	//遍历匹配
 	for(var i=0; i<arr.length; i++){
 		var a = arr[i].split("=");
-		if(a[0].trim() == "userName"){
+		if(a[0].trim() === "userName"){
 			UsernameInfo.innerHTML = a[1];
 		}else{
 			//RoleInfo.innerHTML = a[1];
